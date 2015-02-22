e2ebbab62b655a6f313f93eb9d0aa8bfe76968d8
Tarun Bhardwaj <tarun.bhardwaj@openlabs.co.in>
2015-02-22 11:05:04 +0530

    Fixed falke8 issues

--- tests/test_product.py
+++ tests/test_product.py
@@ -7,17 +7,18 @@
 """
 import sys
 import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(
-    __file__, '..', '..', '..', '..', '..', 'trytond'
-)))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
 import unittest
 
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT
 from trytond.transaction import Transaction
 from decimal import Decimal
+
+DIR = os.path.abspath(os.path.normpath(os.path.join(
+    __file__, '..', '..', '..', '..', '..', 'trytond'
+)))
+if os.path.isdir(DIR):
+    sys.path.insert(0, os.path.dirname(DIR))
 
 
 class TestBase(unittest.TestCase):
